af4bec72d5fa28a508141768da2051b1f1d3f433
cespino93 <chrisl.espino@gmail.com>
2021-03-31 20:45:02 -0400

    Created add a post"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+        @post = Post.new(user_params)
+        if @user.save
+            redirect_to @user
+        else
+          render :new
+        end
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title,:content)
+    end
 end
