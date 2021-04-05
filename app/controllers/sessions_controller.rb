a05a8cdcf1d3bc817461289019599ab9f26b3ffc
cespino93 <chrisl.espino@gmail.com>
2021-04-05 13:48:22 -0400

    Oauth error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,25 +17,23 @@ class SessionsController < ApplicationController
   end
 
   def google
-    
-    # #find_or_create a user using the attributes auth
-    # @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
-    #   user.username = auth["info"]["first_name"]
-    #   user.password = SecureRandom.hex(10)
-    # end
-    # if @user.save
-    #   session[:user_id] = @user.id
-    #   redirect_to user_path(@user)
-    # else
-    #   redirect_to '/'
-    # end
+    #find_or_create a user using the attributes auth
+    @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+      user.name= auth["info"]["first_name"]
+      user.password = SecureRandom.hex(10)
+    end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(user)
+    else
+      redirect_to '/'
+    end
+    binding.pry
   end
 
   private
-
+  
   def auth
     request.env['omniauth.auth']
   end
 end
-
-
